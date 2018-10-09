e6c92d82fbd14be7747c9ba16729afa6c284e757
Suganth Ilangovan <suganthilangovan@gmail.com>
2018-10-09 23:29:39 +0530

    Hashtable implementation (incomplete)

--- hash.cpp
+++ hash.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
-
+#include <list>
 using namespace std;
 
+class IDictionary {
+	virtual void insert(int key, char* value)=0;
+	virtual char* lookup(int key)=0;
+};
+
+class HashTableImpl : public IDictionary {
+private:
+	int size;
+	list<char*>* table;
+
+public:
+	HashTableImpl(int capacity) {
+		size = capacity;
+		table = new list<char*>[size];
+	} // Constructor. Capacity is the table size for the hash range.
+	void insert(int key, char* value) {
+		table[key%size].push_back(value);
+	}
+	char* lookup(int key) {
+		auto it = table[key%size].begin();
+		return *it;
+	}
+};
 int main(){
-	
+	HashTableImpl H(100);
+	H.insert(10, "Suganth");
+	cout << H.lookup(10) << endl;
 	return 0;
 }
